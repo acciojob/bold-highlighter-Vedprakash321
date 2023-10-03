3ee13d87e7162209d64c48986eaabbc80551432b
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-03 20:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Function to highlight bold words in the paragraph
 function highlight() {
-    //Write your code here
-
-
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
 }
 
-
+// Function to revert the color of bold words back to black
 function return_normal() {
-    //Write your code here
-
-    
+    const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 }
+
+// Get the link element by its ID and add event listeners
+const link = document.getElementById('highlight-link');
+link.addEventListener('mouseover', highlight);
+link.addEventListener('mouseout', return_normal);
+
